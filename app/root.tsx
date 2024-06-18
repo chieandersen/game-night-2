6ad99eece28db4fafc7771935c15626012c23173
Chie <chie@nativshark.com>
2024-06-18 12:04:22 +0900

    [files] - initial set up

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,7 +6,29 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+// import "./tailwind.css";
+import tailwindStyleSheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "preload", href: tailwindStyleSheet, as: "style" },
+    // cssBundleHref ? { rel: "preload", href: cssBundleHref, as: "style" } : null,
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+      rel: "stylesheet preconnect crossorigin",
+      href: "https://fonts.gstatic.com",
+      as: "style",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+      as: "style",
+    },
+    // cssBundleHref ? { rel: "stylesheet", href: cssBundleHref } : null,
+    { rel: "stylesheet", href: tailwindStyleSheet },
+  ].filter(Boolean);
+};
+
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -16,7 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="flex font-josefin flex-col w-72 h-svh overflow-hidden bg-pink-300">
         {children}
         <ScrollRestoration />
         <Scripts />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,28 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    fontFamily: {
+      ...fontFamily,
+      josefin: ["Josefin Sans", "Noto Sans JP", ...fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+      },
+    },
   },
+
   plugins: [],
 } satisfies Config;
